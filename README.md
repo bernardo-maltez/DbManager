ba0ffd7f3b5f113b789368d206b5f13b68a4de75
unknown <265664431+bernardo-maltez@users.noreply.github.com>
2026-03-04 20:40:54 -0300

    General code

---
+++ README.md
@@ -0,0 +1,34 @@
+# DB manager
+This is a small program I've written to manage json databases regarding anime, mangas and movies.
+
+## How to make it work?
+1. Extract the folder 
+2. Update the MovieToken and FolderPath inside the config.json
+3. Add the .exe to the system PATH (optional)
+
+## Delted my config file, now what?
+Create a new one and copy the template below
+
+```json
+{
+  "MovieTOKEN" : "YOUR_TMDB_MOVIE_TOKEN",
+  "folderPath" :  "DESIRED PATH, MAKE SURE TO PUT THE FULL PATH"
+}
+```
+## Wanna compile by myself!
+Use [pyInstaller](https://pyinstaller.org/en/stable/usage.html).
+
+## FAQ:
+### Why python?
+Easy to read and mantain.
+
+### Why use this instead of Letterboxd or similar?
+This approach lets you **own** your data. Therefore, if letterboxd goes down or another service becomes more popular it won't affect you.
+
+### Will this pogram receive any major update?
+Probably not.
+
+### Roadmap
+- [X] Code optimaztion (pandas, mainly)
+- [X] Add editing mode 
+- [ ] Add delete mode
---
+++ src/DbManagerCli/search.py
@@ -0,0 +1,75 @@
+import requests
+import DbManagerCli.settings as settings
+
+# Base URLs and headers
+ANILIST_URL = 'https://graphql.anilist.co'
+TMDB_BASE_URL = 'https://api.themoviedb.org/3'
+TMDB_HEADERS = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {settings.MovieTOKEN}"
+}
+GOOGLE_BOOKS_API_URL = 'https://www.googleapis.com/books/v1/volumes?q={}'
+
+def fetch_data(url, headers=None, payload=None):
+    if payload:
+        response = requests.post(url, json=payload, headers=headers)
+    else:
+        response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+        return {'error': f"Error: {response.status_code}, {response.text}"}
+
+def anilist_data(name: str, media_type: str) -> dict:
+    query = """
+        query ($query: String, $format: MediaType, $page: Int, $perpage: Int) {
+            Page (page: $page, perPage: $perpage) {
+                media (search: $query, type: $format) {
+                    title {
+                        romaji
+                        english
+                    }
+                    type
+                    genres
+                    countryOfOrigin
+                    startDate {
+                        year
+                        month
+                        day
+                    }
+                    duration
+                    episodes
+                    chapters
+                }
+            }
+        }
+    """
+    variables = {
+        'query': name,
+        'format': media_type,
+        'page': 1,
+        'perpage': 3
+    }
+    return fetch_data(ANILIST_URL, payload={'query': query, 'variables': variables})
+
+def movie_search(show_name: str) -> dict:
+    url = f"{TMDB_BASE_URL}/search/movie?query={show_name.replace(' ', '%20')}&include_adult=true&page=1"
+    return fetch_data(url, headers=TMDB_HEADERS)
+
+def movie_data(movie_id: int) -> dict:
+    url = f"{TMDB_BASE_URL}/movie/{movie_id}"
+    return fetch_data(url, headers=TMDB_HEADERS)
+
+# def TVShowSearch(show_name: str) -> dict:
+#     url = f"{TMDB_BASE_URL}/search/tv?query={show_name.replace(' ', '%20')}&include_adult=true&page=1"
+#     return fetch_data(url, headers=TMDB_HEADERS)
+
+# def TVShowData(tv_show_id: int) -> dict:
+#     url = f"{TMDB_BASE_URL}/tv/{tv_show_id}"
+#     return fetch_data(url, headers=TMDB_HEADERS)
+
+# def BookSearch(book_name: str) -> dict:
+#     url = GOOGLE_BOOKS_API_URL.format(book_name.replace(' ', '+'))
+#     return fetch_data(url)
